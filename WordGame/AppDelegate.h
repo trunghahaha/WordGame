7409745afc87e8cfba3fcc215e3768138edf8a16
do duc trung <trung_xi_ke_le_be@yahoo.com>
2013-06-11 10:35:50 +0700

    Initial Commit

---
+++ WordGame/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  WordGame
+//
+//  Created by doductrung on 6/11/13.
+//  Copyright (c) 2013 doductrung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ WordGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WordGame
+//
+//  Created by doductrung on 6/11/13.
+//  Copyright (c) 2013 doductrung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
